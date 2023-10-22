8a27d6f340c5f3a2182e811628db1fae34ee6e29
AJAYSINGHBISHT12 <123948333+AJAYSINGHBISHT12@users.noreply.github.com>
2023-10-22 20:21:43 +0530

    Add files via upload

---
+++ day5/datatypes.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int x=10;
+    // long x=15;
+    // cin>>x;
+    int x;
+    cin>>x;
+    cout<<x;
+
+    // long long x = 1500000;
+
+    // string s1;
+    // cin>>s1;
+    // cout<<s1;
+//     string str;
+//     getline(cin,str);
+//     cout<<str;
+//     return 0;
+}
